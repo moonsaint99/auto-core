333e9ce64a0f0017798b15c421522fee4625f6e8
rishi <chanris278@gmail.com>
2023-02-16 14:12:04 -0700

    created importer and main

---
+++ toolbox/importer.py
@@ -0,0 +1,7 @@
+import obspy as op
+
+# The importer reads in SAC data representing differnet stations and events
+# Inside the folder 'datasets', you can add a folder containing your dataset
+# Each dataset folder contains a number of folders corresponding to all events represented in the dataset
+# Each event folder will contain data from all stations that recorded the event
+
